63c33606434b245dba10c95c99ad325ea0524a72
Phithai <phi-thai.lai@chasacademy.se>
2025-09-30 10:18:02 +0200

    backend and server is working locally

---
+++ dist/db.js
@@ -0,0 +1,13 @@
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // Azure kräver SSL, detta fixar det
+    }
+});
+pool.connect()
+    .then(() => console.log("✅ Connected to Azure Postgres"))
+    .catch(err => console.error("❌ Database connection failed:", err));
+export default pool;
--- dist/index.js
+++ dist/index.js
@@ -1,35 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import pool from "./db.ts";
-
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 // routes
 import login from "./routes/login.ts";
 import packages from "./routes/packages.ts";
 import userRoutes from "./routes/user.ts";
-import sendEmail from "./controllers/email.ts";
-
 app.use("/login", login);
 app.use("/packages", packages);
 app.use("/user", userRoutes);
-
 app.get("/", (req, res) => res.send("Hello World"));
-
 // test-api endpoint
 app.get("/test-api", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.json(result.rows);
-  } catch (err) {
-    console.error("❌ Query failed:", err);
-    res.status(500).send("Database error");
-  }
+    try {
+        const result = await pool.query("SELECT NOW()");
+        res.json(result.rows);
+    }
+    catch (err) {
+        console.error("❌ Query failed:", err);
+        res.status(500).send("Database error");
+    }
 });
-
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
