f9a0ed89ef5f77734f266c4251db771389a2f73d
Dennis Granheimer <dennis.granheimer@live.se>
2025-10-17 00:00:25 +0200

    added db migration

---
+++ db/migrations/V2__update_Schema.sql
@@ -0,0 +1,13 @@
+-- Add columns to products table
+ALTER TABLE products
+ADD COLUMN humidity NUMERIC,
+ADD COLUMN delivered BOOLEAN;
+
+-- Remove status column from products table
+ALTER TABLE products
+DROP COLUMN status;
+
+-- Add columns to users table
+ALTER TABLE "users"
+ADD COLUMN password VARCHAR(255),
+ADD COLUMN role VARCHAR(50);
