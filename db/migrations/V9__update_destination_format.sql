485899d9f3b710795811f85e3b981bd22a63241e
TheUnseenBug <dennis.granheimer@live.se>
2025-10-29 11:22:30 +0100

    added new migration

--- db/migrations/V9__update_destination_format.sql
+++ db/migrations/V9__update_destination_format.sql
@@ -1,5 +1,7 @@
 ALTER TABLE packages
-DROP COLUMN destination
+DROP COLUMN IF EXISTS destination,
+DROP COLUMN IF EXISTS thresholds;
 
 ALTER TABLE packages
-ADD COLUMN destination jsonb
+ADD COLUMN destination jsonb,
+ADD COLUMN thresholds jsonb;
